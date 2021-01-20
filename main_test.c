6a9a59334eb946d7ad4a599939a46ae64e7d4e89
omar rashad salem <77698890+orsnaro@users.noreply.github.com>
2021-01-20 15:55:43 +0200

    Add files via upload

---
+++ main_test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int row_num,colm_num;
+    
+        printf("PLEASE! choose Array 'A' Rank (maximum 4x4)==>\n\n");
+        printf("  A Rank= \n\n"
+       
+        scanf("%d",&row_num);
+        if( scanf("%d", &row_num)!=1 || row_num>4||row_num<0)
+            printf("daasf");
+}
